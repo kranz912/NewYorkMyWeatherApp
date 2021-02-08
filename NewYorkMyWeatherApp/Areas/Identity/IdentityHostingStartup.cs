1a92e7140cede7c98b6809a7f204fde8de24542f
kranz nikolai fontanilla <me@kranznikolai.com>
2021-02-08 15:36:37 -0500

    Add project files.

---
+++ NewYorkMyWeatherApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NewYorkMyWeatherApp.Data;
+using NewYorkMyWeatherApp.Models;
+
+[assembly: HostingStartup(typeof(NewYorkMyWeatherApp.Areas.Identity.IdentityHostingStartup))]
+namespace NewYorkMyWeatherApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
